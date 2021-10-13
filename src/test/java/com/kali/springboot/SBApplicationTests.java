9c3ce774d12f0efbd1dd701038b4bdb1d950e217
Elliot <2271998412@qq.com>
2021-10-13 14:46:52 +0800

    引入aop包 去除log4j2包的版本号 不然maven无法找到

--- src/test/java/com/kali/springboot/SBApplicationTests.java
+++ src/test/java/com/kali/springboot/SBApplicationTests.java
@@ -1,13 +1,16 @@
 package com.kali.springboot;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Log4j2
 @SpringBootTest
 class SBApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("测试方法输出:contextLoads===>{}", "SpringBootTest");
     }
 
 }
