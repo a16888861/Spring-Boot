579f4fc4ddda4a014d8e8957993864330b1ca94c
a16888861 <2271998412@qq.com>
2021-10-24 00:48:48 +0800

    更新记录： 1.更改上传接口为批量上传文件 2.新增添加修改文件备注或删除文件信息共用接口

--- src/main/java/com/kali/blog/controller/file/SysFileUploadController.java
+++ src/main/java/com/kali/blog/controller/file/SysFileUploadController.java
@@ -1,14 +1,18 @@
 package com.kali.blog.controller.file;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import com.github.xiaoymin.knife4j.annotations.ApiSupport;
 import com.kali.blog.common.aspect.Log;
 import com.kali.blog.common.base.BaseController;
 import com.kali.blog.common.base.CommonPage;
+import com.kali.blog.common.context.UserContextUtil;
 import com.kali.blog.common.response.Response;
 import com.kali.blog.common.response.ResponseEnum;
 import com.kali.blog.common.response.ResponseInfo;
+import com.kali.blog.dto.SysFileUploadDTO;
+import com.kali.blog.entity.SysFileUpload;
 import com.kali.blog.service.SysFileUploadService;
 import com.kali.blog.vo.SysFileUploadVO;
 import com.kali.blog.vo.SysFileUploadVOPage;
@@ -16,6 +20,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -26,6 +31,8 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * 系统-文件上传 前端控制器
@@ -44,9 +51,9 @@ public class SysFileUploadController extends BaseController {
     private SysFileUploadService service;
 
     /**
-     * 上传文件
+     * 批量上传文件
      *
-     * @param file     文件
+     * @param fileList 文件
      * @param filePath 要上传的文件路径
      * @return 上传结果
      */
@@ -55,10 +62,17 @@ public class SysFileUploadController extends BaseController {
     @ApiOperation(value = "上传文件", notes = "上传文件接口")
     @ApiOperationSupport(author = "Elliot", order = 0)
     public ResponseInfo<Response> uploadFile(
-            @RequestPart(value = "file") MultipartFile file,
+            @RequestPart(value = "fileList") List<MultipartFile> fileList,
             @ApiParam(value = "文件上传路径", type = "String", required = true) String filePath) {
+        if (StringUtils.isBlank(filePath)) {
+            return Response.fail("file.filePath.isEmpty");
+        }
+        if (fileList.size() > 5) {
+            return Response.fail("file.size.isTooMuch");
+        }
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        return judgeResult(service.uploadFile(request, file, filePath));
+        fileList.forEach(file -> service.uploadFile(request, file, filePath));
+        return judgeResult(1);
     }
 
     /**
@@ -98,5 +112,34 @@ public class SysFileUploadController extends BaseController {
             return Response.notFound("common.response.notfound");
         }
         return Response.success(ResponseEnum.SUCCESS.getMessage(), selectFilePage);
+    }
+
+    /**
+     * 添加修改文件备注或删除文件信息共用接口
+     *
+     * @param id     文件ID
+     * @param remark 备注
+     * @param del    删除状态
+     * @return 更改结果
+     */
+    @Log("添加修改文件备注或删除文件信息")
+    @PutMapping(value = "deleteAndRemark")
+    @ApiOperation(value = "添加修改文件备注或删除文件信息", notes = "添加修改文件备注或删除文件信息接口")
+    @ApiOperationSupport(author = "Elliot", order = 3)
+    public ResponseInfo<Response> deleteAndRemark(@ApiParam(value = "文件ID", required = true) String id,
+                                                  @ApiParam(value = "说明", required = false) String remark,
+                                                  @ApiParam(value = "删除状态", required = false) String del) {
+        if (StringUtils.isBlank(id)) {
+            return Response.fail("file.id.isEmpty");
+        }
+        SysFileUploadDTO dto = SysFileUploadDTO.builder().updateBy(UserContextUtil.getUserInfo().getId()).updateDate(LocalDateTime.now()).build();
+        if (StringUtils.isNotBlank(remark)) {
+            dto.setFileRemark(remark);
+        }
+        if (StringUtils.isNotBlank(del)) {
+            dto.setDelFlag(del);
+        }
+        int result = service.update(dto, new LambdaQueryWrapper<SysFileUpload>().eq(SysFileUpload::getId, id));
+        return judgeResult(result);
     }
 }
